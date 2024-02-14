31c72b28be778478c5afb488bde06ead9ba22a39
Oliver Frost <oliverfrost11@gmail.com>
2024-02-14 10:30:13 +0100

    Added documentation for IPostEntityProcessingService

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -4,9 +4,16 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
- *
+ * Used to process logic after the entity logic has been processed.
  */
 public interface IPostEntityProcessingService {
 
+    /**
+     * Called after the entity processing has been done.
+     * @param gameData
+     * @param world
+     * @precondition The entity processing has been done.
+     * @postcondition The post entity processing has been done.
+     */
     void process(GameData gameData, World world);
 }
