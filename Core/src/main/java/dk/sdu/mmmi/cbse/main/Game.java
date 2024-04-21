180833ac4c176ebf998647bf9a27e9105c209f1a
Oliver Frost <oliverfrost11@gmail.com>
2024-04-21 10:37:30 +0200

    Added Spring implementation for Core module

---
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Game.java
@@ -0,0 +1,169 @@
+package dk.sdu.mmmi.cbse.main;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author Oliver Frost
+ */
+public class Game {
+    private final GameData gameData = new GameData();
+    private final World world = new World();
+    private Pane gameWindow;
+    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
+    private final List<IGamePluginService> gamePluginServices;
+    private final List<IEntityProcessingService> entityProcessingServiceList;
+    private final List<IPostEntityProcessingService> postEntityProcessingServices;
+
+    Game(List<IGamePluginService> gamePluginServices, List<IEntityProcessingService> entityProcessingServices, List<IPostEntityProcessingService> postEntityProcessingServices) {
+      this.gamePluginServices = gamePluginServices;
+      this.entityProcessingServiceList = entityProcessingServices;
+      this.postEntityProcessingServices = postEntityProcessingServices;
+    }
+
+    public void start(Stage window) throws Exception {
+        window.setResizable(false);
+        Text text = new Text(10, 20, "Destroyed asteroids: 0");
+        gameWindow = new Pane();
+        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
+        gameWindow.getChildren().add(text);
+
+        Scene scene = new Scene(gameWindow);
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, true);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, true);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, true);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, true);
+            }
+        });
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode().equals(KeyCode.LEFT)) {
+                gameData.getKeys().setKey(GameKeys.LEFT, false);
+            }
+            if (event.getCode().equals(KeyCode.RIGHT)) {
+                gameData.getKeys().setKey(GameKeys.RIGHT, false);
+            }
+            if (event.getCode().equals(KeyCode.UP)) {
+                gameData.getKeys().setKey(GameKeys.UP, false);
+            }
+            if (event.getCode().equals(KeyCode.SPACE)) {
+                gameData.getKeys().setKey(GameKeys.SPACE, false);
+            }
+
+        });
+
+        // Lookup all Game Plugins using ServiceLoader
+        for (IGamePluginService iGamePlugin : getGamePluginServices()) {
+            iGamePlugin.start(gameData, world);
+        }
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
+            polygons.put(entity, polygon);
+            gameWindow.getChildren().add(polygon);
+        }
+
+        render();
+
+        window.setScene(scene);
+        window.setTitle("ASTEROIDS");
+        window.show();
+    }
+
+    public void render() {
+
+
+        new AnimationTimer() {
+            private long then = 0;
+
+
+
+            @Override
+            public void handle(long now) {
+                update();
+                draw();
+                gameData.getKeys().update();
+            }
+
+        }.start();
+    }
+
+    private void update() {
+
+
+
+        // Update
+        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
+            entityProcessorService.process(gameData, world);
+        }
+        for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
+            postEntityProcessorService.postProcess(gameData, world);
+        }
+    }
+
+    private void draw() {
+
+        // For each entity in world, add to polygons and gameWindow to show them on screen
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
+            if(!polygons.containsKey(entity)) {
+                polygons.put(entity, polygon);
+
+                gameWindow.getChildren().add(polygon);
+            }
+        }
+
+        // Remove entities that are not in world anymore
+        polygons.forEach((key,value) -> {
+            if(!world.getEntities().contains(key)){
+                gameWindow.getChildren().remove(value);
+                polygons.remove(key);
+            }
+        });
+
+        // Standard draw logic
+        for (Entity entity : world.getEntities()) {
+            Polygon polygon = polygons.get(entity);
+            polygon.setTranslateX(entity.getX());
+            polygon.setTranslateY(entity.getY());
+            polygon.setRotate(entity.getRotation());
+        }
+    }
+
+    private Collection<IGamePluginService> getGamePluginServices() {
+        return gamePluginServices;
+    }
+    private Collection<IEntityProcessingService> getEntityProcessingServices() {
+        return entityProcessingServiceList;
+    }
+
+    private Collection<IPostEntityProcessingService> getPostEntityProcessingServices() {
+        return postEntityProcessingServices;
+    }
+}
