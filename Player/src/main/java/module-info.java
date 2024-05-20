928a0d382bf6c8442cde00529bd25eedee8edebd
Oliver Frost <oliverfrost11@gmail.com>
2024-05-20 10:23:52 +0200

    Fix: Removed unnecessary exports directive in module-info.java in Player

--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -3,11 +3,9 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import dk.sdu.mmmi.cbse.commonbullet.BulletSPI;
 
 module Player {
-    exports dk.sdu.mmmi.cbse.playersystem;
     requires Common;
     requires CommonBullet;
     uses BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
-
 }
